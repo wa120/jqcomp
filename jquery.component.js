fd3d5ad77c2440e8b594e7221a770779c132b42f
Wei-Ta,Wang <wa114040@gmail.com>
2015-10-02 19:25:12 +0800

    Update jquery.component.js

--- jquery.component.js
+++ jquery.component.js
@@ -1,7 +1,8 @@
 /*******
 The MIT License (MIT)
 
-Copyright (c) 2015 Wei Ta Wang
+Copyright (c) 2015 Wei Ta Wang SmartAll inc. 
+http://jqcomp.org/
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
