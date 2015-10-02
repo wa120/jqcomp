d53dfe249dce3a8383e751d6e070246b96cdbe00
Wei-Ta,Wang <wa114040@gmail.com>
2015-10-02 14:49:01 +0800

    Update jquery.component.js

--- jquery.component.js
+++ jquery.component.js
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 *****/
 (function($){
-  //ajax loading seqence
-  //View >> Model >> Controller
 
   var DefaultPath="packages";
   var Driver="main";
